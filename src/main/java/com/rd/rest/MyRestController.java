f0713df1b403c061a18df4ee59e938a16c1b0de8
Rohit-Datir-Barii <rohitdatir143@gmail.com>
2023-05-04 21:25:23 +0530

    adding annotation

--- src/main/java/com/rd/rest/MyRestController.java
+++ src/main/java/com/rd/rest/MyRestController.java
@@ -1,5 +1,6 @@
 package com.rd.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/spring")
 public class MyRestController {
 	
+	@GetMapping("/msg")
 	public String getMesage() {
 		return "Welcome to Docker";
 	}
